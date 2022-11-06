4bf7a80d6b9474df7184983e378f81e32f332908
PedroLopes7K <pedrohenri3601@gmail.com>
2022-11-06 11:06:56 -0300

    add form for create burger

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,154 @@
+<template>
+  
+<div>
+  <p>msg component</p>
+
+  <div>
+    <form id="burger-form">
+   <div class="input-container">
+    <label for="name">Name of customer:</label>
+    <input type="text" id="name" name="name" v-model="name">
+   </div>
+   <div class="input-container">
+    <label for="bread">Choice the bread:</label>
+    <select name="bread" id="bread" v-model="bread">
+      <option value="">-----</option>
+
+      <option v-for="bread in breads" :key="bread.id" :value="bread.tipo"> {{ bread.tipo}}</option>
+    </select>
+   </div>
+
+   <div class="input-container">
+    <label for="meat">Choice the meat:</label>
+    <select name="meat" id="meat" v-model="meat">
+      <option value="">-----</option>
+      <option v-for="meat in meats" :key="meat.id" :value="meat.tipo"> {{ meat.tipo}}</option>
+    </select>
+   </div>
+
+   
+   <div id="options-container" class="input-container">
+    <label id="options-title" for="meat">Choice options:</label>
+    <div class="checkbox-container" v-for="option in optionsData" :key="option.id" >
+      <input type="checkbox" name="options" v-model="opitions" :value="option.value" >
+      <span>{{option.tipo}}</span>
+    </div>
+    
+   </div>
+
+   
+   <div class="input-container">
+<input type="submit" class="submit-btn" value="Create Burger">
+   </div>
+    </form>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "BurgerForm",
+  data() {
+    return {
+      breads: null,
+      meats: null,
+      optionsData: null,
+      name:null,
+      bread: null,
+      meat: null,
+      options: [],
+      status: "Send",
+      msg: null
+    }
+  },
+
+  methods: {
+    async getIngredients() {
+      const req = await fetch('http://localhost:3000/ingredientes')
+      const data = await req.json()
+
+      console.log(data)
+
+      this.breads = data.paes
+      this.meats = data.carnes
+      this.optionsData = data.opcionais
+
+    }
+  },
+
+  mounted() {
+    this.getIngredients()
+  }
+}
+</script>
+
+<style scoped>
+
+#burger-form {
+  max-width: 400px;
+  margin: 0 auto;
+}
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 15px;
+  color: #222;
+  padding: 5px 10px;
+  border-left: 4px solid #FCBA03;
+}
+
+input, select {
+  padding: 5px, 10px;
+width: 300px;
+height: 30px;
+}
+
+#options-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+#options-title {
+  width: 100%;
+}
+.checkbox-container {
+  display: flex;
+  align-items: center;
+}
+.checkbox-container span {
+  margin-left: 6px;
+  font-weight: bold;
+}
+
+.checkbox-container span,
+.checkbox-container input {
+  width: auto;
+  margin: 4px;
+}
+
+
+.submit-btn {
+  background-color: #222;
+  color: #FCBA03;
+  font-weight: bold;
+  height: 50px;
+  border: 2px solid #222;
+  border-radius: 5px;
+  padding: 5px 5px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: .5s;
+}
+
+.submit-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,20 @@
   <Banner />
   <div class="main-container">
   <h1>Make Your Burger:</h1>
+  <BurgerForm/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
 export default {
   name: 'HomeView',
+
   components: {
-    Banner
+    Banner,
+    BurgerForm
   }
 }
 </script>
