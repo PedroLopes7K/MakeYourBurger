4ce95879b52674a65277324254dcb8e78a66c14d
PedroLopes7K <pedrohenri3601@gmail.com>
2022-11-07 19:11:24 -0300

    create dashboard of requests

--- src/views/RequestView.vue
+++ src/views/RequestView.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="main-container">
-    <h1>This is an request page</h1>
+    <h1>Managments Requests:</h1>
+      <Dashboard />
   </div>
 </template>
 
 
 <script>
+  import Dashboard from '../components/Dashboard'
 
 export default {
   name: 'RequestView',
   components: {
+    Dashboard
   }
 }
 </script>
